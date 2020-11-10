378703b44284cf5f81ee92328c71febe5452c3ba
MehrajShakil <54067524+MehrajShakil@users.noreply.github.com>
2020-11-10 12:28:26 +0600

    Update Tamplet.cpp

--- Modular Inverse/Tamplet.cpp
+++ Modular Inverse/Tamplet.cpp
@@ -2,7 +2,7 @@ Src : fsshakkhor ( CF ) vaiya.
 
 struct Combinatorics{
     ll fac[N],inv[N];
- 
+
     ll BigMod(ll b,ll p,ll m)
     {
         ll ret = 1;
@@ -13,18 +13,21 @@ struct Combinatorics{
         }
         return ret;
     }
- 
+
     void init(){
         fac[0] = 1;
         for(int i = 1;i < N;i++)fac[i] = (fac[i - 1] * i) % MOD;
         for(int i = 0;i < N;i++)inv[i] = BigMod(fac[i],MOD-2,MOD);
     }
- 
+
     ll com(ll n,ll r)
     {
         if(n < r)return 0;
         ll u = fac[n];
         ll v = (inv[r] * inv[n - r]) % MOD;
         return (u * v) % MOD;
+    }
+    ll FAC ( ll n ){ /// added.
+       return fac[n];
     }
 }combi;
